03a3d475b21f5ae490492d927daf4a5795e1e73e
Nico Krause <inspiraluna@gmail.com>
2016-05-04 11:23:42 +0200

    changed mobile-config.js

---
+++ one2many-call/mobile-config.js
@@ -0,0 +1,33 @@
+App.info({
+  id: 'de.lespace.kurentometeor',
+  name: 'one2manyCall',
+  description: 'a simple Kurento WebRTC Example',
+  author: 'Nico Krause',
+  email: 'nico@le-space.de',
+  website: 'http://www.le-space.de'
+});
+
+// Set up resources such as icons and launch screens.
+// App.icons({
+//   'iphone': 'icons/icon-60.png',
+//   'iphone_2x': 'icons/icon-60@2x.png',
+//   // ... more screen sizes and platforms ...
+// });
+
+// App.launchScreens({
+//   'iphone': 'splash/Default~iphone.png',
+//   'iphone_2x': 'splash/Default@2x~iphone.png',
+//   // ... more screen sizes and platforms ...
+// });
+
+// Set PhoneGap/Cordova preferences
+// App.setPreference('BackgroundColor', '0xff0000ff');
+
+
+App.accessRule('http://*');
+App.accessRule('https://*');
+App.accessRule('*');
+App.accessRule('*://173.194.71.127:*');
+App.accessRule('*5.9.154.226:*');
+App.accessRule('*://localhost:*');
+App.accessRule('*://192.168.192.251:*');  
